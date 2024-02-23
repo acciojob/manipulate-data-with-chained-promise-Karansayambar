808b62e9977443f94ee491d71d616eb8e688a7bc
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-23 08:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+	let arr = [1,2,3,4];
+	let output = document.getElementById("output");
+	let promise1 = (inputArry) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+			resolve(inputArry);
+			}, 3000);
+		});
+	};
+
+	promise1(arr)
+	.then(filteredData => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				let evenNumbers = filteredData.filter(item => item % 2 === 0);
+			    resolve(evenNumbers);
+			},0);
+		});
+	})
+	.then(evenNumbers => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				let multiply = evenNumbers.map(item => item * 2);
+			output.innerHTML += multiply.join(",");
+			resolve(multiply);
+			},0);
+		})
+	})
+	.catch(err => {
+		console.log(err);
+	})
+	});
